8ca14edee5aa5762cf1b6aa30b8aa2e4b0569e6f
Ryan1729 <Ryan1729@gmail.com>
2025-03-17 01:06:00 -0600

    add configuration

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-struct GummyBear;
+mod minimal {
+    use std::io::Write;
 
-type Minimal = Option<GummyBear>;
+    struct GummyBear;
 
-fn main() {
-    let mut study: Minimal = None;
+    type Minimal = Option<GummyBear>;
 
-    println!("{}", study.is_some());
+    pub fn run(mut w: impl Write) -> Result<(), std::io::Error> {
+        let mut study: Minimal = None;
 
-    study = Some(GummyBear);
+        writeln!(w, "{}", study.is_some())?;
 
-    println!("{}", study.is_some());
+        study = Some(GummyBear);
 
-    study = None;
+        writeln!(w, "{}", study.is_some())?;
 
-    println!("{}", study.is_some());
+        study = None;
+
+        writeln!(w, "{}", study.is_some())?;
+
+        Ok(())
+    }
+}
+
+#[derive(serde::Deserialize)]
+enum Mode {
+    Minimal,
+}
+
+#[derive(serde::Deserialize)]
+struct Spec {
+    mode: Mode,
+}
+
+type Res<A> = Result<A, Box<dyn std::error::Error>>;
+
+fn main() -> Res<()> {
+    use Mode::*;
+    let spec: Spec = get_spec()?;
+
+    let output = std::io::stdout();
+
+    match spec.mode {
+        Minimal => {
+            minimal::run(&output)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn get_spec() -> Res<Spec> {
+    // TODO accept CLI args to indicate a config file to overwrite the default config
+    Ok(config::Config::builder()
+        .add_source(config::File::with_name("config").required(false))
+        .add_source(config::Environment::with_prefix("FIST"))
+        .build()?
+        .try_deserialize::<Spec>()?)
 }
